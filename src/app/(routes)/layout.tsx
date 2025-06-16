73c0584da8b4b2c45db4f256fae75065cc55655b
Daniel Xavier <dxdanielxavier@gmail.com>
2025-06-16 17:10:16 -0300

    Adjusted some styles

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="inline-grid justify-self-center min-w-80 md:min-w-100 px-8 py-12 gap-y-6 border border-transparent rounded-3xl shadow-xl bg-[#fdfcfc]">
+    <div className="grid gap-y-6 min-w-80 md:min-w-100 px-8 py-12 rounded-3xl shadow-xl bg-[#fdfcfc]">
       {children}
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,12 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className="w-vw h-vh">
         <header className="fixed top-0 right-0 left-0 border-b text-center py-2 shadow-xl">
-          <Link href="/">Tasks App</Link>
+          <Link className="font-bold" href="/">
+            Tasks App
+          </Link>
         </header>
 
-        <main className="mt-24 mb-14 h-full flex items-start justify-center">
+        <main className="mt-24 mb-14 flex items-start justify-center">
           {children}
         </main>
 
